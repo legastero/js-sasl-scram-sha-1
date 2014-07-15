d2030769c5233979adeb26fefd8698f115c3d542
Lance Stout <lancestout@gmail.com>
2014-07-14 22:15:23 -0700

    Simplify everything and fix browserify issues

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # SASL : SCRAM-SHA-1
 
+[![Build Status](https://travis-ci.org/otalk/xmpp-uri.png)](https://travis-ci.org/otalk/xmpp-uri)
+[![Dependency Status](https://david-dm.org/otalk/xmpp-uri.png)](https://david-dm.org/otalk/xmpp-uri)
+[![devDependency Status](https://david-dm.org/otalk/xmpp-uri/dev-status.png)](https://david-dm.org/otalk/xmpp-uri#info=devDependencies)
+
+[![Browser Support](https://ci.testling.com/otalk/xmpp-uri.png)](https://ci.testling.com/otalk/xmpp-uri)
+
+
 This module is a JavaScript implementation of the SCRAM-SHA-1 SASL mechanism,
 which plugs into the [SASL](https://github.com/jaredhanson/js-sasl) framework.
 
-## Install
+## Installing
 
-##### npm
-
-    $ npm install sasl-scram-sha-1
-
-##### volo
-
-    $ volo add legastero/js-sasl-scram-sha-1 sasl-scram-sha-1
-
-For more information on using volo to manage JavaScript modules, visit [http://volojs.org/](http://volojs.org/).
+```sh
+$ npm install sasl-scram-sha-1
+```
 
 ## Usage
 
@@ -38,43 +39,7 @@ var secondResp = mech.challenge('r="XCV234BAL90",s="XMXC234DFS",i=4096')
 Currently missing features:
 
 - Mutual authentication of the server based on the success message.
-
-## Compatibility
-
-##### Browser
-
-This module is [AMD](https://github.com/amdjs/amdjs-api)-compliant, and can be
-loaded by module loaders such as [RequireJS](http://requirejs.org/).
-
-##### Node
-
-This module is compatible with [Node](http://nodejs.org/).
-
-## Tests
-
-##### Browser
-
-To run tests in a browser, execute the Make target for the desired browser:
-
-    $ make test-chrome
-    $ make test-firefox
-    $ make test-safari
-
-##### PhantomJS
-
-To run headless tests from a terminal using [PhantomJS](http://phantomjs.org/):
-
-    $ make test-phantomjs
-
-##### Node
-
-To run tests in Node:
-
-    $ make test-node
-    
-##### Status
-
-[![Travis CI](https://secure.travis-ci.org/legastero/js-sasl-scram-sha-1.png)](http://travis-ci.org/legastero/js-sasl-scram-sha-1)
+  
 
 ## Credits
 
---
+++ index.js
@@ -0,0 +1,108 @@
+var bitops = require('./lib/bitops');
+var utils = require('./lib/utils');
+
+var RESP = {};
+
+
+function Mechanism(options) {
+    options = options || {};
+    this._genNonce = options.genNonce || utils.genNonce;
+    this._stage = 'initial';
+}
+
+// Conform to the SASL lib's expectations
+Mechanism.Mechanism = Mechanism;
+
+
+Mechanism.prototype.name = 'SCRAM-SHA-1';
+Mechanism.prototype.clientFirst = true;
+
+
+Mechanism.prototype.response = function (cred) {
+    return RESP[this._stage](this, cred);
+};
+
+Mechanism.prototype.challenge = function (chal) {
+    var values = utils.parse(chal);
+
+    this._salt = new Buffer(values.s || '', 'base64').toString('binary');
+    this._iterationCount = parseInt(values.i, 10);
+    this._nonce = values.r;
+    this._verifier = values.v;
+    this._error = values.e;
+    this._challenge = chal;
+
+    return this;
+};
+
+
+RESP.initial = function (mech, cred) {
+    mech._cnonce = mech._genNonce();
+
+    var authzid = '';
+    if (cred.authzid) {
+        authzid = 'a=' + utils.saslname(cred.authzid);
+    }
+
+    mech._gs2Header = 'n,' + authzid + ',';
+
+    var nonce = 'r=' + mech._cnonce;
+    var username = 'n=' + utils.saslname(cred.username);
+
+    mech._clientFirstMessageBare = username + ',' + nonce;
+    var result = mech._gs2Header + mech._clientFirstMessageBare;
+
+    mech._stage = 'challenge';
+
+    return result;
+};
+
+
+RESP.challenge = function (mech, cred) {
+    var gs2Header = new Buffer(mech._gs2Header).toString('base64');
+
+    mech._clientFinalMessageWithoutProof = 'c=' + gs2Header + ',r=' + mech._nonce;
+
+    var saltedPassword, clientKey, serverKey;
+    if (cred.clientKey && cred.serverKey) {
+        clientKey = cred.clientKey;
+        serverKey = cred.serverKey;
+    } else {
+        saltedPassword = cred.saltedPassword || bitops.Hi(cred.password, mech._salt, mech._iterationCount);
+        clientKey = bitops.HMAC(saltedPassword, 'Client Key');
+        serverKey = bitops.HMAC(saltedPassword, 'Server Key');
+    }
+
+    var storedKey = bitops.H(clientKey);
+    var authMessage = mech._clientFirstMessageBare + ',' +
+                      mech._challenge + ',' +
+                      mech._clientFinalMessageWithoutProof;
+    var clientSignature = bitops.HMAC(storedKey, authMessage);
+
+    var xorstuff = bitops.XOR(clientKey, clientSignature);
+
+    var clientProof = new Buffer(xorstuff, 'binary').toString('base64');
+
+    mech._serverSignature = bitops.HMAC(serverKey, authMessage);
+
+    var result = mech._clientFinalMessageWithoutProof + ',p=' + clientProof;
+
+    mech._stage = 'final';
+
+    mech.cache = {
+        saltedPassword: saltedPassword,
+        clientKey: clientKey,
+        serverKey: serverKey
+    };
+
+    return result;
+};
+
+RESP.final = function () {
+    // TODO: Signal errors
+    return '';
+};
+
+
+
+module.exports = Mechanism;
